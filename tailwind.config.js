67619000ec1ca85d17c85243a946934a887dc5a6
Oussama Aouina <oussamaaouina99@gmail.com>
2024-12-01 22:16:28 +0100

    setting up the libraries and plugins configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,18 @@ module.exports = {
   content: ["./App.{js,ts,jsx,tsx}", "./Screens/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "greenBlue-100": "#05F2DB",
+        "greenBlue-200": "#1ED9D9",
+        "greenBlue-300": "#0F97A6",
+        "greenBlue-400": "#0E6973",
+        "greenBlue-500": "#0A3A40",
+        purpleBlue: "#030A8C",
+        "dark-500": "2B2B2B",
+        "dark-200": "5C5C5C",
+      },
+    },
   },
   plugins: [],
 };
